2d3ff0e76ae535b884c5a17ae9aab8806978cc3e
zhangtl125 <zhangtl125@sina.cn>
2018-04-21 12:55:00 +0800

    第三次提交

--- Practicess/src/cn/cn/Student.java
+++ Practicess/src/cn/cn/Student.java
@@ -2,4 +2,15 @@ package cn.cn;
 
 public class Student {
 
+	public int id;
+
+	public int getId() {
+		System.out.println("id");
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }
